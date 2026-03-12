0324c5d79883f1b1117354f4f649c37cdf90868f
Kelechi Ebiri <ebiritg@gmail.com>
2026-03-13 00:18:44 +0100

    refactor: update main function to use async and initialize telemetry

--- wallet-service/src/main.rs
+++ wallet-service/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use wallet_service::{configuration::get_configuration, startup::Application, telemetry};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    telemetry::init_subscriber(telemetry::get_subscriber(
+        "wallet-service".into(),
+        "info".into(),
+        std::io::stdout,
+    ));
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+
+    Ok(())
 }

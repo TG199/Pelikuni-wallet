db3e6be729b680af4ce47393820bce69cb8a7efc
Kelechi Ebiri <ebiritg@gmail.com>
2026-03-17 17:12:39 +0100

    feat: implement application startup with configuration and server initialization

---
+++ wallet-service/src/startup.rs
@@ -0,0 +1,93 @@
+use actix_web::cookie::Key;
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
+use reqwest::Url;
+use secrecy::ExposeSecret;
+use secrecy::SecretString;
+use serde::Deserialize;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
+
+use crate::configuration::DatabaseSettings;
+use crate::configuration::Settings;
+
+
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
+        let connection_pool = get_connection_pool(&configuration.database);
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
+
+        let listener = TcpListener::bind(address);
+
+        let port = listener.local_addr().unwrap().port();
+
+        let server = run(
+            listener,
+            connection_pool,
+            configuration.application.url().expect("Invalid host url"),
+            configuration.application.hmac_secret,
+        )
+        .await?;
+        Ok(Self {port, server})
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.with_db)
+}
+
+pub struct ApplicationBaseUrl(pub Url);
+
+async fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    base_url: Url,
+    hmac_secret: SecretString,
+) -> Result<Server, anyhow::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(RequestId)
+            .wrap(TracingLogger::default())
+            .app_data(web::JsonConfig::default().limit(262_144))
+            .app_data(web::PayloadConfig::default().limit(10_485_760))
+            .route("/", web::get().to(home))
+            .route("/health", web::get().to(health_check))
+            .
+            .app_data(db_pool.clone())
+            .app_data(base_url.clone())
+            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
+
+#[derive(Clone, Deserialize)]
+pub struct HmacSecret(pub SecretString);
